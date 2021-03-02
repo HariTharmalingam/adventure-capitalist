19350ae784fc570dd5171a330937cc2643d041af
xen <xzen769@gmail.com>
2021-03-02 15:10:56 +0100

    feat(home): add initialstate and connect product component

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container, Row } from 'react-bootstrap';
 
+import initalState from './inital-state';
 import Product from '../product';
 import BarrePrice from '../barre-price';
 
@@ -8,13 +9,8 @@ const Home = () => (
   <Container className="bg-dark" style={{ height: '100%' }}>
     <BarrePrice />
     <Row>
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
+      {initalState.map((data) => <Product key={data.name} data={data} />)}
+      {/* [<Product data={{ totalSell: 1 }} />, <Product data={{ price: 2 }} />] */}
     </Row>
   </Container>
 );
---
+++ src/components/home/inital-state.js
@@ -0,0 +1,7 @@
+export default [{
+  multi: 1,
+  name: 'lemon',
+  price: 1,
+  timeFactory: 1000,
+  totalCostFactoryUnit: 2.65
+}];
