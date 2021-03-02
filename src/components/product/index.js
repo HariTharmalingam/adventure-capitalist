3253751c42e1d39013a0d821f46958b93dbdbb35
xen <xzen769@gmail.com>
2021-03-02 15:26:42 +0100

    feat(product): compute multi price and factory cost

--- src/components/product/index.js
+++ src/components/product/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   Row,
   Col,
@@ -6,44 +6,81 @@ import {
   ProgressBar
 } from 'react-bootstrap';
 
-import car from './images/car.png';
+import lemon from './images/lemon.png';
 
-const Product = () => (
-  <Col md={6}>
-    <Row>
-      <Col md={4}>
+const Product = class Product extends Component {
+  constructor(props) {
+    super(props);
+
+    console.log(props);
+
+    const {
+      multi,
+      name,
+      price,
+      timeFactory,
+      totalCostFactoryUnit
+    } = props.data;
+
+    this.state = {
+      name,
+      price: multi * price,
+      multi,
+      totalSell: 0,
+      timeFactory,
+      totalCostFactory: multi * totalCostFactoryUnit,
+      totalCostFactoryUnit: multi * totalCostFactoryUnit
+    };
+  }
+
+  render() {
+    const {
+      price,
+      multi,
+      totalSell,
+      totalCostFactory,
+      totalCostFactoryUnit
+    } = this.state;
+    const totalMoney = 2650;
+
+    return (
+      <Col md={6}>
         <Row>
-          <Col md={12}>
-            <img src={car} className="shadow p-4 bg-info rounded-circle img-fluid" alt="product" />
+          <Col md={4}>
+            <Row>
+              <Col md={12}>
+                <img src={lemon} className="shadow p-4 bg-info rounded-circle img-fluid" alt="product" />
+              </Col>
+              <Col md={12}>
+                <h2 className="text-center text-white">{`${price} $`}</h2>
+              </Col>
+            </Row>
           </Col>
-          <Col md={12}>
-            <h2 className="text-center text-white">{`${10.0} $`}</h2>
+          <Col md={8}>
+            <Row>
+              <Col md={12}>
+                <Alert variant="primary">
+                  <h1 className="display-6">{`${totalSell} $`}</h1>
+                </Alert>
+              </Col>
+            </Row>
+            <Row className="mb-3">
+              <Col md={12}>
+                <ProgressBar variant="info" now={((totalMoney * 100) / totalCostFactory)} />
+              </Col>
+            </Row>
+            <Row>
+              <Col md={12}>
+                <Alert variant="warning">
+                  <h1 className="display-6">{`Buy X${multi} : ${totalCostFactoryUnit} $`}</h1>
+                </Alert>
+              </Col>
+            </Row>
           </Col>
         </Row>
       </Col>
-      <Col md={8}>
-        <Row>
-          <Col md={12}>
-            <Alert variant="primary">
-              <h1 className="display-6">{`${10.0} $`}</h1>
-            </Alert>
-          </Col>
-        </Row>
-        <Row className="mb-3">
-          <Col md={12}>
-            <ProgressBar variant="info" now={(((10 * 10) / 100) * 100)} />
-          </Col>
-        </Row>
-        <Row>
-          <Col md={12}>
-            <Alert variant="warning">
-              <h1 className="display-6">{`Buy X1 : ${2.65} $`}</h1>
-            </Alert>
-          </Col>
-        </Row>
-      </Col>
-    </Row>
-  </Col>
-);
+    );
+  }
+};
 
 export default Product;
